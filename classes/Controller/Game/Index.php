d744a5a36422b7683c5ef965b3b6105f633d1555
Hinton <hinton.oscar@gmail.com>
2013-07-24 01:54:13 +0200

    Initial commit.

---
+++ classes/Controller/Game/Index.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class Controller_Game_Index extends MG_Controller_Game_Index {}
---
+++ classes/Controller/Game/LuckyWheel.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class Controller_Game_LuckyWheel extends MG_Controller_Game_LuckyWheel {}
